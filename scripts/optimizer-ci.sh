821875228251c333da450c3ea5a3b561d698127d
Mauro Lacy <mauro@lacy.com.es>
2024-02-13 18:24:54 +0100

    Run optimizer with volume

--- scripts/optimizer-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-U="cosmwasm"
-V="0.15.0"
-
-docker run --rm -v "$(pwd)":/code \
-  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
-  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  $U/optimizer:$V
